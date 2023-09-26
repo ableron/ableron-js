a215c515ffee37069dbb3a311eea45bc923e5266
Torsten <dev@t-blasche.de>
2023-09-26 23:38:40 +0200

    find includes

---
+++ src/include.ts
@@ -0,0 +1,61 @@
+export class Include {
+  /**
+   * Name of the attribute which contains the ID of the include - an optional unique name.
+   */
+  private readonly ATTR_ID: string = 'id';
+
+  /**
+   * Name of the attribute which contains the source URl to resolve the include to.
+   */
+  private readonly ATTR_SOURCE: string = 'src';
+
+  /**
+   * Name of the attribute which contains the timeout for requesting the src URL.
+   */
+  private readonly ATTR_SOURCE_TIMEOUT_MILLIS: string = 'src-timeout-millis';
+
+  /**
+   * Name of the attribute which contains the fallback URL to resolve the include to in case the
+   * source URL could not be loaded.
+   */
+  private readonly ATTR_FALLBACK_SOURCE: string = 'fallback-src';
+
+  /**
+   * Name of the attribute which contains the timeout for requesting the fallback-src URL.
+   */
+  private readonly ATTR_FALLBACK_SOURCE_TIMEOUT_MILLIS: string = 'fallback-src-timeout-millis';
+
+  /**
+   * Name of the attribute which denotes a fragment whose response code is set as response code
+   * for the page.
+   */
+  private readonly ATTR_PRIMARY: string = 'primary';
+
+  /**
+   * Raw include tag.
+   */
+  private readonly rawIncludeTag: string;
+
+  /**
+   * Raw attributes of the include tag.
+   */
+  private readonly rawAttributes: Map<string, string>;
+
+  /**
+   * Fallback content to use in case the include could not be resolved.
+   */
+  private readonly fallbackContent: string | undefined;
+
+  /**
+   * Constructs a new Include.
+   *
+   * @param rawAttributes Raw attributes of the include tag
+   * @param fallbackContent Fallback content to use in case the include could not be resolved
+   * @param rawIncludeTag Raw include tag
+   */
+  constructor(rawAttributes: Map<string, string>, fallbackContent?: string, rawIncludeTag?: string) {
+    this.rawIncludeTag = typeof rawIncludeTag !== 'undefined' ? rawIncludeTag : '';
+    this.rawAttributes = rawAttributes;
+    this.fallbackContent = typeof fallbackContent !== 'undefined' ? fallbackContent : '';
+  }
+}
--- src/transclusion-processor.ts
+++ src/transclusion-processor.ts
@@ -1,11 +1,30 @@
 import { AbleronConfig } from './ableron-config';
 import { TransclusionResult } from './transclusion-result';
+import { Include } from './include';
 
 export class TransclusionProcessor {
+  /**
+   * Regular expression for matching ableron includes.
+   */
+  private readonly INCLUDE_PATTERN: RegExp = /<(ableron-include)\s(([^">]|"[^"]*")*?)(\/>|>(.*?)<\/\1>)/g;
+
   private readonly ableronConfig: AbleronConfig;
 
   constructor(ableronConfig: AbleronConfig) {
     this.ableronConfig = ableronConfig;
+  }
+
+  findIncludes(content: string): Set<Include> {
+    const firstIncludePosition = content.indexOf('<ableron-include');
+
+    return firstIncludePosition === -1
+      ? new Set()
+      : new Set(
+          Array.from(
+            content.matchAll(this.INCLUDE_PATTERN),
+            (match) => new Include(new Map<string, string>(), match[5], match[0])
+          )
+        );
   }
 
   resolveIncludes(content: string, presentRequestHeaders: Map<string, string[]>): TransclusionResult {
@@ -13,9 +32,10 @@ export class TransclusionProcessor {
     const transclusionResult = new TransclusionResult(content, this.ableronConfig.statsAppendToContent);
 
     //TODO: Implement
-    transclusionResult.addResolvedInclude();
-    // findIncludes().map (include => include.resolve());...
-
+    this.findIncludes(content).forEach((include) => {
+      transclusionResult.addResolvedInclude();
+      console.log(include);
+    });
     transclusionResult.setProcessingTimeMillis(Date.now() - startTime);
     return transclusionResult;
   }
