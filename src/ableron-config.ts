c8b25a0f99a8cf7811ba20e34fe5011145db8713
Torsten <dev@t-blasche.de>
2023-10-05 09:05:45 +0200

    added missing config parameters

--- src/ableron-config.ts
+++ src/ableron-config.ts
@@ -10,6 +10,33 @@ export class AbleronConfig {
   readonly fragmentRequestTimeoutMillis: number = 3000;
 
   /**
+   * Request headers that are passed to fragment requests if present.
+   */
+  readonly fragmentRequestHeadersToPass = [
+    'Accept-Language',
+    'Correlation-ID',
+    'Forwarded',
+    'Referer',
+    'User-Agent',
+    'X-Correlation-ID',
+    'X-Forwarded-For',
+    'X-Forwarded-Proto',
+    'X-Forwarded-Host',
+    'X-Real-IP',
+    'X-Request-ID'
+  ];
+
+  /**
+   * Response headers of primary fragments to pass to the page response if present.
+   */
+  readonly primaryFragmentResponseHeadersToPass = ['Content-Language', 'Location', 'Refresh'];
+
+  /**
+   * Maximum size in bytes the fragment cache may have.
+   */
+  readonly cacheMaxSizeInBytes = 1024 * 1024 * 10;
+
+  /**
    * Whether to append UI composition stats as HTML comment to the content.
    */
   readonly statsAppendToContent: boolean = false;
--- test/ableron-config.test.ts
+++ test/ableron-config.test.ts
@@ -7,5 +7,20 @@ test('should have default value for each property', () => {
   // then
   expect(config.enabled).toBe(true);
   expect(config.fragmentRequestTimeoutMillis).toBe(3000);
+  expect(config.fragmentRequestHeadersToPass).toEqual([
+    'Accept-Language',
+    'Correlation-ID',
+    'Forwarded',
+    'Referer',
+    'User-Agent',
+    'X-Correlation-ID',
+    'X-Forwarded-For',
+    'X-Forwarded-Proto',
+    'X-Forwarded-Host',
+    'X-Real-IP',
+    'X-Request-ID'
+  ]);
+  expect(config.primaryFragmentResponseHeadersToPass).toEqual(['Content-Language', 'Location', 'Refresh']);
+  expect(config.cacheMaxSizeInBytes).toBe(10485760);
   expect(config.statsAppendToContent).toBe(false);
 });
