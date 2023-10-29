659e67a04d1dd407687b304b62fae16b84bf6b2d
Torsten <dev@t-blasche.de>
2023-10-29 23:07:54 +0100

    export AbstractLogger

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Ableron } from './ableron';
 import { AbleronConfig } from './ableron-config';
+import { AbstractLogger } from './abstract-logger';
 import { TransclusionResult } from './transclusion-result';
 
-export { Ableron, AbleronConfig, TransclusionResult };
+export { Ableron, AbleronConfig, AbstractLogger, TransclusionResult };
